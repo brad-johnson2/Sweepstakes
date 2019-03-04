00812354f9ecc524e50818796500b29e2c37272d
brad-johnson2 <46530051+brad-johnson2@users.noreply.github.com>
2019-03-04 08:58:07 -0600

    init commit

---
+++ Sweepstake/Sweepstakes/Contestant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sweepstakes
+{
+    public class Contestant
+    {
+        Contestant[] contestant = new Contestant[4];
+        
+
+        string firstNamee;
+        string lastName;
+        string emailAddress;
+        int regNumber;
+    }
+
+    public void DisplayEmployeeIdName()
+    {
+        Dictionary<int, string> contestants = new Dictionary<int, string>();
+        contestants.Add(1337, "Terrill");
+        contestants.Add(1234, "Heinisch");
+        contestants.Add(9541, "Carlson");
+
+        Console.Write("Enter an employee name: ");
+        string value = Console.ReadLine();
+        foreach (KeyValuePair<int, string> contestant in contestants)
+        {
+            if (contestant.Value == value)
+                Console.WriteLine($"Employee ID: {contestant.Key} Employee Name: {contestant.Value}");
+        }
+    }
+}
---
+++ Sweepstake/Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Sweepstakes
+    {
+        public List<Contestant> contestants = new List<Contestant>();
+
+
+        public Sweepstakes(string name)
+        {
+
+        }
+
+        public void RegisterContestant(Contestant contestant)
+        {
+            contestants.Add(contestant);
+
+        }
+        public string PickWinner()
+        {
+            Random rnd = new Random();
+            int winningNumber = rnd.Next(0, contestants.Count);
+            string winner = contestants[winningNumber];
+            return winner;
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            Console.WriteLine(contestant);
+        }
+    }
+}
